e8b5069e01f6790a9e601350d0101e0001ee7d8b
joaomarcosvs <joaomarcos.vieirasantos@gmail.com>
2025-12-23 11:12:23 -0300

    user and resouce

--- src/main/java/com/estudobetha/course/CourseApplication.java
+++ src/main/java/com/estudobetha/course/CourseApplication.java
@@ -3,7 +3,7 @@ package com.estudobetha.course;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.estudobetha")
 public class CourseApplication {
 
 	public static void main(String[] args) {
