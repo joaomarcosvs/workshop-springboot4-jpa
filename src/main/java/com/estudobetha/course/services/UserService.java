8020f00043bac5674d498887b629d82c4c270f7c
joaomarcosvs <joaomarcos.vieirasantos@gmail.com>
2025-12-26 09:23:42 -0300

    service layer

---
+++ src/main/java/com/estudobetha/course/services/UserService.java
@@ -0,0 +1,25 @@
+package com.estudobetha.course.services;
+
+import com.estudobetha.course.entities.User;
+import com.estudobetha.course.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll() {
+        return repository.findAll();
+    }
+
+    public User findeById(Long id) {
+        Optional<User> obj = repository.findById(id);
+        return obj.get();
+    }
+}
