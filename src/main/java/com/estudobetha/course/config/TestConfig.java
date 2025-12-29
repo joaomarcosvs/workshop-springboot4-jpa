3ab86358292bb5f78094818c9328b6db1ef3378f
joaomarcosvs <joaomarcos.vieirasantos@gmail.com>
2025-12-29 11:06:02 -0300

    continuing last commit

--- src/main/java/com/estudobetha/course/config/TestConfig.java
+++ src/main/java/com/estudobetha/course/config/TestConfig.java
@@ -1,12 +1,15 @@
 package com.estudobetha.course.config;
 
+import com.estudobetha.course.entities.Order;
 import com.estudobetha.course.entities.User;
+import com.estudobetha.course.repositories.OrderRepository;
 import com.estudobetha.course.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import java.time.Instant;
 import java.util.Arrays;
 
 @Configuration
@@ -16,12 +19,20 @@ public class TestConfig implements CommandLineRunner {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private OrderRepository orderRepository;
+
     @Override
     public void run(String... args) throws Exception {
 
         User u1 = new User(null, "Luiza", "luiza@gmail.com", "999999999", "123456");
         User u2 = new User(null, "Joao M", "joaom@gmail.com", "888888888", "123456");
 
+        Order o1 = new Order(null, u1, Instant.parse("2019-06-20T19:53:07Z"));
+        Order o2 = new Order(null, u2, Instant.parse("2019-07-21T03:42:10Z"));
+        Order o3 = new Order(null, u1, Instant.parse("2019-07-22T15:21:22Z"));
+
         userRepository.saveAll(Arrays.asList(u1, u2));
+        orderRepository.saveAll(Arrays.asList(o1, o2, o3));
     }
 }
--- src/main/java/com/estudobetha/course/resources/OrderResource.java
+++ src/main/java/com/estudobetha/course/resources/OrderResource.java
@@ -1,7 +1,7 @@
 package com.estudobetha.course.resources;
 
-import com.estudobetha.course.entities.User;
-import com.estudobetha.course.services.UserService;
+import com.estudobetha.course.entities.Order;
+import com.estudobetha.course.services.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,21 +12,21 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping (value = "/users")
-public class UserResource {
+@RequestMapping (value = "/orders")
+public class OrderResource {
 
     @Autowired
-    private UserService service;
+    private OrderService service;
 
     @GetMapping
-    public ResponseEntity<List<User>> findAll() {
-        List<User> list = service.findAll();
+    public ResponseEntity<List<Order>> findAll() {
+        List<Order> list = service.findAll();
         return ResponseEntity.ok().body(list);
     }
 
     @GetMapping(value = "/{id}")
-    public ResponseEntity<User> findById(@PathVariable Long id){
-        User obj = service.findeById(id);
+    public ResponseEntity<Order> findById(@PathVariable Long id){
+        Order obj = service.findeById(id);
         return ResponseEntity.ok().body(obj);
     }
 }
